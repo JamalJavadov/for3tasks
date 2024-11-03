1c4fc9a7514471d75ebde2f0f570b8beda1268b5
JamalJavadov <camalcavadovwork@gmail.com>
2024-11-03 21:55:25 +0400

    task 2 added

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
 class Solution {
-    public boolean isPalindrome(int x) {
-        int reversedvalue=0;
-        int x2=x;
-        while(x>0){
-            reversedvalue= x%10 + reversedvalue*10;
-            x/=10;
-        }
-        if(reversedvalue==x2){
-            return true;
+    public int smallestEvenMultiple(int n) {
+        if(n%2==0){
+            return n;
         }else{
-            return false;
+            return n*2;
         }
-
     }
 }
